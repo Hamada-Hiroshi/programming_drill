7f256166408ec4e555281a65834e604f54b28715
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-06-07 08:10:03 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  get 'languages/show'
-  get 'reviews/index'
-  get 'questions/index'
-  get 'questions/edit'
-  get 'learnings/show'
-  get 'learnings/edit'
-  get 'apps/index'
-  get 'apps/new'
-  get 'apps/confirm'
-  get 'apps/show'
-  get 'apps/edit'
-  get 'apps/add_edit'
-  get 'apps/hint'
-  get 'apps/explanation'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/quit'
-  get 'home/about'
+  root 'apps#index'
+  get 'about' => 'home#about'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/:id/quit' => 'users#quit', as: 'quit_user'
+  patch 'users/:id/canel' => 'users#cancel', as: 'cancel_user'
+  resources :users, only: [:show, :edit, :update]
+
+  get 'apps/rate' => 'apps#rate_index', as: 'rate_apps'
+  post 'apps/confirm' => 'apps#confirm', as: 'confirm_apps'
+  get 'apps/:id/add_edit' => 'apps#add_edit', as: 'add_edit_app'
+  get 'apps/:id/hint' => 'apps#hint', as: 'hint_app'
+  get 'apps/:id/explanation' => 'apps#explanation', as: 'explanation_app'
+  resources :apps, only: [:new, :create, :show, :edit, :update] do
+    resources :learnings, only: [:show, :edit, :update]
+    resources :questions, only: [:index, :edit, :update]
+    resource :reviews, only: [:index]
+  end
+
+  get 'languages/:id/rate' => 'languages#rate_show', as: 'rate_language'
+  resources :languages, only: [:show]
 end
