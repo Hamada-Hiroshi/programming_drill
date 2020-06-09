403b3250c1f511018777903aa8cdd0e6e36e92c1
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-06-09 05:31:40 +0000

    [Fix]ストロングパラメータ微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def show
   end
 
@@ -26,10 +30,6 @@ class UsersController < ApplicationController
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def user_params
     params.require(:user).permit(:name, :email, :profile_image, :introduction, :skill)
   end
