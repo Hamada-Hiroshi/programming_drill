1139e137a7146dfdbcb7f3b2f5b81ca461c98d90
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-06-11 09:16:16 +0000

    [Add]質問機能追加（返信は未実装）

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,27 @@
 class QuestionsController < ApplicationController
   def index
+    @app = App.find(params[:app_id])
+    @learning = Learning.find_by(user_id: current_user.id, app_id: @app.id)
+    @question = Question.new
+    @questions = @app.questions.all
   end
 
-  def edit
+  def create
+    @app = App.find(params[:app_id])
+    @question = current_user.questions.new(question_params)
+    @question.app_id = @app.id
+    if @question.save
+      flash[:success] = "質問を投稿しました。"
+      redirect_to app_questions_path(@app)
+    else
+      @learning = Learning.find_by(user_id: current_user.id, app_id: @app.id)
+      @questions = @app.questions.all
+      render 'index'
+    end
   end
 
-  def update
+  private
+  def question_params
+    params.require(:question).permit(:content)
   end
 end
