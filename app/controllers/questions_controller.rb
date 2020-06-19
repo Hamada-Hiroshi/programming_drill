e4c7786f0c999fb9fae17136a034532691559895
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-06-19 10:47:38 +0000

    [Update]質問投稿 非同期通信

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,16 +12,17 @@ class QuestionsController < ApplicationController
     @app = App.find(params[:app_id])
     @question = current_user.questions.new(question_params)
     @question.app_id = @app.id
+    @questions = @app.questions.where(parent_id: nil).order(created_at: "DESC")
     if @question.save
       if @question.parent_id == nil
         flash[:success] = "質問を投稿しました。"
       else
         flash[:success] = "質問への回答を投稿しました。"
       end
-      redirect_to app_questions_path(@app)
+      @question = Question.new
+
     else
       @learning = Learning.find_by(user_id: current_user.id, app_id: @app.id)
-      @questions = @app.questions.where(parent_id: nil).order(created_at: "DESC")
       render 'index'
     end
   end
