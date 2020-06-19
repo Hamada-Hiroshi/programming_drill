63c2f62c4ba028ee1ddaddabcf89ffde6ad8e7a8
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-06-19 00:27:01 +0000

    [Add]アプリ名検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def set_search
+    @q = App.ransack(params[:q])
+  end
 
   protected
   def configure_permitted_parameters
