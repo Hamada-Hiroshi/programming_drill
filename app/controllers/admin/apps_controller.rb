7369d6b0c3180871d340b69af1e34d629917bd6d
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-07-10 07:24:42 +0000

    [Add]管理者アプリページ作成・表示項目修正

--- app/controllers/admin/apps_controller.rb
+++ app/controllers/admin/apps_controller.rb
@@ -6,5 +6,9 @@ class Admin::AppsController < ApplicationController
   end
 
   def cancel
+    @app = App.find(params[:id])
+    @app.update(status: false)
+    flash[:success] = "指定のアプリを非公開にしました。"
+    redirect_back(fallback_location: admin_root_path)
   end
 end
