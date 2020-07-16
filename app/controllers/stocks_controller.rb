90e59d2f0404a53414991135f5967abeb39d4286
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-07-16 04:30:28 +0000

    [Add]ストック機能非同期通信

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,13 @@
 class StocksController < ApplicationController
   def create
+    @app = App.find(params[:app_id])
     stock = current_user.stocks.build(app_id: params[:app_id])
     stock.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @app = App.find(params[:app_id])
     stock = current_user.stocks.find_by(app_id: params[:app_id])
     stock.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
