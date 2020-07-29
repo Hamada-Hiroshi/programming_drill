02aefd0e2474ed630c6bc1d90d0190b2909acda8
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-07-29 08:55:40 +0000

    [Add]管理者ページ 検索・ソート機能追加

--- app/controllers/admin/apps_controller.rb
+++ app/controllers/admin/apps_controller.rb
@@ -2,7 +2,8 @@ class Admin::AppsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @apps = App.includes(:user, :lang).page(params[:page]).per(10)
+    @app_q = App.ransack(params[:q])
+    @apps = @app_q.result(distinct: true).includes(:user, :lang).page(params[:page]).per(10)
   end
 
   def cancel
