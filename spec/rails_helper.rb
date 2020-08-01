68d6906102b315ebbeed8232f721c27ca10c4e3d
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-08-01 08:21:14 +0000

    [Add]RSpec User関連コントローラ・統合テスト追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ Faker::Config.locale = :ja
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -39,6 +39,10 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     # driven_by :selenium_chrome_headless
     driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,59 +1,59 @@
 require 'rails_helper'
 
-describe 'ユーザ認証のテスト' do
-  context 'ユーザ新規登録' do
-    before do
-      visit new_user_registration_path
+RSpec.describe "Users", type: :system do
+  describe 'ユーザ認証のテスト' do
+    context 'ユーザ新規登録' do
+      before do
+        visit new_user_registration_path
+      end
+
+      it '新規登録に成功する' do
+        fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        click_button '登録する'
+        expect(current_path).to eq user_path(1)
+      end
+      it '新規登録に失敗する' do
+        fill_in 'user[name]', with: ''
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        fill_in 'user[password_confirmation]', with: ''
+        click_button '登録する'
+        expect(page).to have_content '正しく入力されていない項目があります'
+      end
     end
 
-    it '新規登録に成功する' do
-      fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
-      fill_in 'user[email]', with: Faker::Internet.email
-      fill_in 'user[password]', with: 'password'
-      fill_in 'user[password_confirmation]', with: 'password'
-      click_button '登録する'
-      expect(current_path).to eq(user_path(1))
-    end
-    it '新規登録に失敗する' do
-      fill_in 'user[name]', with: ''
-      fill_in 'user[email]', with: ''
-      fill_in 'user[password]', with: ''
-      fill_in 'user[password_confirmation]', with: ''
-      click_button '登録する'
-      expect(page).to have_content '正しく入力されていない項目があります'
+    context 'ユーザログイン' do
+      let(:test_user) { create(:user) }
+      before do
+        visit new_user_session_path
+      end
+
+      it 'ログインに成功する' do
+        fill_in 'user[email]', with: test_user.email
+        fill_in 'user[password]', with: test_user.password
+        click_button 'ログイン'
+        expect(current_path).to eq user_path(test_user)
+      end
+      it 'ログインに失敗する' do
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'ログイン'
+        expect(current_path).to eq new_user_session_path
+      end
     end
   end
 
-  context 'ユーザログイン' do
+  describe '詳細画面のテスト' do
+    let(:lang) { create(:lang) }
     let(:test_user) { create(:user) }
-    before do
-      visit new_user_session_path
-    end
+    let!(:post_app) { create(:app, user: test_user, lang: lang) }
+    let(:test_user_2) { create(:user) }
+    let(:learning_app) { create(:app, user: test_user_2, lang: lang) }
+    let!(:learning) { create(:learning, user: test_user, app: learning_app) }
 
-    it 'ログインに成功する' do
-      fill_in 'user[email]', with: test_user.email
-      fill_in 'user[password]', with: test_user.password
-      click_button 'ログイン'
-      expect(current_path).to eq(user_path(test_user))
-    end
-    it 'ログインに失敗する' do
-      fill_in 'user[email]', with: ''
-      fill_in 'user[password]', with: ''
-      click_button 'ログイン'
-      expect(current_path).to eq(new_user_session_path)
-    end
-  end
-end
-
-describe 'ユーザのテスト' do
-  let(:lang) { create(:lang) }
-  let(:test_user) { create(:user) }
-  let!(:post_app) { create(:app, user: test_user, lang: lang) }
-  let(:test_user_2) { create(:user) }
-  let(:learning_app) { create(:app, user: test_user_2, lang: lang) }
-  let!(:learning) { create(:learning, user: test_user, app: learning_app) }
-
-  context '詳細画面のテスト' do
     it 'ログインユーザの詳細ページ' do
       sign_in test_user
       visit user_path(test_user)
@@ -71,19 +71,16 @@ describe 'ユーザのテスト' do
 
       aggregate_failures do
         expect(page).not_to have_link 'アカウント情報を編集', href: edit_user_path(test_user)
-        expect(page).not_to have_content(learning_app.title)
-        expect(page).to have_content(post_app.title)
+        expect(page).not_to have_content learning_app.title
+        expect(page).to have_content post_app.title
         expect(page).not_to have_link '内容を編集する', href: edit_app_path(post_app)
       end
     end
-
-    it 'ログインしていない場合ログインページへリダイレクト' do
-      visit user_path(test_user)
-      expect(current_path).to eq(new_user_session_path)
-    end
   end
 
-  context '編集画面のテスト' do
+  describe '編集画面のテスト' do
+    let(:test_user) { create(:user) }
+
     it 'ログインユーザの編集ページ' do
       sign_in test_user
       visit edit_user_path(test_user)
@@ -95,16 +92,102 @@ describe 'ユーザのテスト' do
         expect(page).to have_link 'アカウント停止', href: quit_user_path(test_user)
       end
     end
+  end
 
-    it '他のユーザの場合ページ遷移できない' do
-      sign_in test_user_2
+  describe 'ユーザ情報更新のテスト' do
+    let(:test_user) { create(:user) }
+
+    it 'ログインユーザのプロフィール更新' do
+      sign_in test_user
       visit edit_user_path(test_user)
-      expect(current_path).to eq(root_path)
+
+      fill_in "ユーザーネーム", with: "new_user_name"
+      fill_in "自己紹介", with: "test_user_introduction"
+      fill_in "スキル", with: "test_user_skill"
+      click_button "更新する"
+
+      aggregate_failures do
+        expect(page).to have_current_path user_path(test_user)
+        expect(page).to have_content "new_user_name"
+        expect(page).to have_content "test_user_introduction"
+        expect(page).to have_content "test_user_skill"
+      end
     end
 
-    it 'ログインしていない場合ページ遷移できない' do
-      visit edit_user_path(test_user)
-      expect(current_path).to eq(new_user_session_path)
+    xit 'ログインユーザのパスワード変更' do
+      sign_in test_user
+      visit edit_user_registration_path
+
+      fill_in "現在のパスワード", with: "password"
+      fill_in "新しいパスワード", with: "newpassword"
+      fill_in "新しいパスワード（確認用）", with: "newpassword"
+      click_button "パスワードを変更"
+
+      aggregate_failures do
+        expect(page).to have_current_path root_path
+        expect(test_user.reload.password).to eq "newpassword"
+      end
+    end
+
+    it 'ログインユーザのアカウント停止', js: true do
+      sign_in test_user
+      visit quit_user_path(test_user)
+
+      click_link "退会する"
+      expect(page.driver.browser.switch_to.alert.text).to eq "退会してもよろしいですか？"
+      page.driver.browser.switch_to.alert.accept
+
+      aggregate_failures do
+        expect(page).to have_current_path new_user_session_path
+        expect(test_user.reload.status).to eq false
+      end
+
+      sign_in test_user
+
+      aggregate_failures do
+        expect(page).to have_current_path new_user_session_path
+        expect(page).to have_content "アカウントが有効化されていません。"
+      end
+    end
+  end
+
+  describe '学習アプリ画面・ストックアプリ画面のテスト' do
+    let(:lang) { create(:lang) }
+    let(:test_user) { create(:user) }
+    let(:test_user_2) { create(:user) }
+    let(:test_app) { create(:app, user: test_user_2, lang: lang) }
+    let(:test_app_2) { create(:app, user: test_user_2, lang: lang) }
+    let(:test_app_3) { create(:app, user: test_user_2, lang: lang) }
+    let!(:learning) { create(:learning, user: test_user, app: test_app) }
+    let!(:learned) { create(:learning, user: test_user, app: test_app_2, status: "学習済み") }
+    let!(:learned_2) { create(:learning, user: test_user, app: test_app_3, status: "学習済み") }
+    let!(:reviewed) { create(:review_2, user: test_user, app: test_app_3) }
+    let!(:stock_1) { create(:stock, user: test_user, app: test_app) }
+    let!(:stock_2) { create(:stock, user: test_user, app: test_app_2) }
+
+    it 'ログインユーザの学習アプリ一覧ページ' do
+      sign_in test_user
+      visit learnings_user_path(test_user)
+
+      aggregate_failures do
+        expect(page).to have_link test_app.title, href: app_path(test_app)
+        expect(page).to have_content "学習中"
+        expect(page).to have_content "学習済み"
+        expect(page).to have_link "レビューを投稿する", href: app_reviews_path(test_app_2)
+        expect(page).to have_content "レビュー済み"
+      end
+    end
+
+    it 'ログインユーザのストックアプリ一覧ページ' do
+      sign_in test_user
+      visit stocks_user_path(test_user)
+
+      aggregate_failures do
+        expect(page).to have_link test_app.title, href: app_path(test_app)
+        expect(page).to have_link, href: app_stocks_path(test_app)
+        expect(page).to have_link test_app_2.title, href: app_path(test_app_2)
+        expect(page).to have_link, href: app_stocks_path(test_app_2)
+      end
     end
   end
 end
