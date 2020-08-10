e9badd0b8c70668e0e3b4c9446900b11826dc19d
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-08-10 06:51:43 +0000

    [fix]RSpec Users統合テスト修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,50 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  describe 'ユーザ認証のテスト' do
-    context 'ユーザ新規登録' do
-      before do
-        visit new_user_registration_path
-      end
-
-      it '新規登録に成功する' do
-        fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
-        fill_in 'user[email]', with: Faker::Internet.email
-        fill_in 'user[password]', with: 'password'
-        fill_in 'user[password_confirmation]', with: 'password'
-        click_button '登録する'
-        expect(current_path).to eq user_path(1)
-      end
-      it '新規登録に失敗する' do
-        fill_in 'user[name]', with: ''
-        fill_in 'user[email]', with: ''
-        fill_in 'user[password]', with: ''
-        fill_in 'user[password_confirmation]', with: ''
-        click_button '登録する'
-        expect(page).to have_content '正しく入力されていない項目があります'
-      end
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_user_registration_path
     end
 
-    context 'ユーザログイン' do
-      let(:test_user) { create(:user) }
-      before do
-        visit new_user_session_path
-      end
+    it '新規登録に成功する' do
+      fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
+      fill_in 'user[email]', with: Faker::Internet.email
+      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button '登録する'
+      expect(current_path).to eq user_path(1)
+    end
 
-      it 'ログインに成功する' do
-        fill_in 'user[email]', with: test_user.email
-        fill_in 'user[password]', with: test_user.password
-        click_button 'ログイン'
-        expect(current_path).to eq user_path(test_user)
-      end
-      it 'ログインに失敗する' do
-        fill_in 'user[email]', with: ''
-        fill_in 'user[password]', with: ''
-        click_button 'ログイン'
-        expect(current_path).to eq new_user_session_path
-      end
+    it '新規登録に失敗する' do
+      fill_in 'user[name]', with: ''
+      fill_in 'user[email]', with: ''
+      fill_in 'user[password]', with: ''
+      fill_in 'user[password_confirmation]', with: ''
+      click_button '登録する'
+      expect(page).to have_content '正しく入力されていない項目があります'
     end
   end
+
+  describe 'ユーザログインのテスト' do
+    let(:test_user) { create(:user) }
+    before do
+      visit new_user_session_path
+    end
+
+    it 'ログインに成功する' do
+      fill_in 'user[email]', with: test_user.email
+      fill_in 'user[password]', with: test_user.password
+      click_button 'ログイン'
+      expect(current_path).to eq user_path(test_user)
+    end
+
+    it 'ログインに失敗する' do
+      fill_in 'user[email]', with: ''
+      fill_in 'user[password]', with: ''
+      click_button 'ログイン'
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+
 
   describe '詳細画面のテスト' do
     let(:lang) { create(:lang) }
