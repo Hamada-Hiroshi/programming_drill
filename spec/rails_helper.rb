0d1cda57066340feff3a2057c26d89839ffcb295
はまひつじ <r.m.c.f.789@gmail.com>
2020-08-12 19:36:16 +0900

    [Update]CircleCI 設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,19 +10,6 @@ require 'selenium-webdriver'
 require 'devise'
 Faker::Config.locale = :ja
 
-Capybara.register_driver :remote_chrome do |app|
-  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-    "goog:chromeOptions" => {
-      "args" => [
-        "no-sandbox",
-        "headless",
-        "disable-gpu",
-        "window-size=1680,1050"
-      ]
-    }
-  )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, url: url, desired_capabilities: caps)
-end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -92,4 +79,19 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
+end
+
+Capybara.register_driver :remote_chrome do |app|
+  url = "http://chrome:4444/wd/hub"
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {
+      "args" => [
+        "no-sandbox",
+        "headless",
+        "disable-gpu",
+        "window-size=1680,1050"
+      ]
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
 end
